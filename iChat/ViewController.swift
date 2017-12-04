ff9353d445bdf46d2f5ff23415b11c1904eb6fee
ernestodebesto <filip.sikor@gmail.comgitgit config --global user.email filip.sikor@gmail.com>
2017-12-04 15:06:20 +0100

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Filip Sikora on 04/12/2017.
+//  Copyright © 2017 Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
